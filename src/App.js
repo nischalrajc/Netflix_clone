9572afc890d4d0e53a095f0560de79470d833e95
nischalrajc <nischalraj22@gmail.com>
2023-10-18 16:33:10 +0530

    netflix final draft

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css'
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
 import RowPost from './Components/RowPost/RowPost';
+import { originals,action } from './urls';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+
     </div>
   );
 }
